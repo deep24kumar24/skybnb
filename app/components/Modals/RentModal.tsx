51d5ac549fc92c926679a95d5ff4a91d7eb32653
Deepak <deep24kumar24@gmail.com>
2023-11-20 23:56:23 -0500

    Added 3 steps for skybnb your home

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -6,6 +6,9 @@ import Heading from "../Heading";
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../Inputs/CountrySelect";
+import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
 
 enum STEPS {
 	CATEGORY = 0,
@@ -43,6 +46,18 @@ const RentModal = () => {
 	});
 
 	const category = watch("category");
+	const location = watch("location");
+	const guestCount = watch("guestCount");
+	const roomCount = watch("roomCount");
+	const bathroomCount = watch("bathroomCount");
+
+	const Map = useMemo(
+		() =>
+			dynamic(() => import("../Map"), {
+				ssr: false,
+			}),
+		[location]
+	);
 
 	const setCustomValue = (id: string, value: any) => {
 		setValue(id, value, {
@@ -99,17 +114,73 @@ const RentModal = () => {
 		</div>
 	);
 
+	if (step === STEPS.LOCATION) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Where is your place located?"
+					subtitle="Help guests find you!"
+				/>
+
+				<CountrySelect
+					value={location}
+					onChange={(value) =>
+						setCustomValue("location", value)
+					}
+				/>
+
+				<Map center={location?.latlng} />
+			</div>
+		);
+	}
+
+	if (step === STEPS.INFO) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Share some basics about your place"
+					subtitle="What amenities you have?"
+				/>
+				<Counter
+					title="Guests"
+					subtitle="How many guets do you allow?"
+					value={guestCount}
+					onChange={(value) =>
+						setCustomValue("guestCount", value)
+					}
+				/>
+				<Counter
+					title="Rooms"
+					subtitle="How many rooms do you have?"
+					value={roomCount}
+					onChange={(value) =>
+						setCustomValue("roomCount", value)
+					}
+				/>
+
+				<Counter
+					title="Bathrooms"
+					subtitle="How many bathrooms do you have?"
+					value={bathroomCount}
+					onChange={(value) =>
+						setCustomValue("bathroomCount", value)
+					}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={rentModal.onClose}
+			onSubmit={onNext}
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={
 				step === STEPS.CATEGORY ? undefined : onBack
 			}
-			title="Skybnb your Home"
+			title="Skybnb your Home!"
 			body={bodyContent}
 		/>
 	);
